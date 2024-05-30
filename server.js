8b227299eb25c7e776977cd82a19fcfab68f710c
Girijajethitech <girijashankarmohanta11@gmail.com>
2024-05-30 21:51:55 +0530

    todo app server file add

---
+++ server.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const app = express();
+const port = 3001;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+const mongoDBUri = process.env.MONGODB_URI;
+
+mongoose.connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const taskSchema = new mongoose.Schema({
+    task: String
+});
+        
+const Task = mongoose.model('Task', taskSchema);
+
+// API endpoints
+
+// Get all tasks
+app.get('/tasks', async (req, res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+
+// Add new task
+app.post('/tasks', async (req, res) => {
+    const newTask = new Task(req.body);
+    await newTask.save();
+    res.json(newTask);
+});
+
+// Delete task
+app.delete('/tasks/:id', async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Task deleted' });
+});
+
+// Edit task
+app.put('/tasks/:id', async (req, res) => {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedTask);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
